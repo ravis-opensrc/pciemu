764498239159a2b83feb4e9b9017b835f7c7b716
Ravi Shankar <ravis.opensrc@micron.com>
2025-01-09 15:57:59 -0800

    Trace events for pciemu mmio read/write
    Signed-off-by: Ravi Shankar <ravis.opensrc@micron.com>

---
+++ src/hw/pciemu/trace.h
@@ -0,0 +1,5 @@
+#include "trace/trace-hw_misc_pciemu.h"
+
+/*Generate this file if it is not created automatically.
+ * ./scripts/tracetool.py --backend=log,simple,ust --group=hw_misc_pciemu --format=h hw/misc/pciemu/trace-events trace/trace-hw_misc_pciemu.h
+*/
