89f1ea1f412db5bf5e8e270e25569c28e4c87c8d
Ravi Shankar <ravis.opensrc@micron.com>
2025-01-09 23:05:44 -0800

    Removing unnecessary comment
    Signed-off-by: Ravi Shankar <ravis.opensrc@micron.com>

--- src/hw/pciemu/trace.h
+++ src/hw/pciemu/trace.h
@@ -1,5 +1,1 @@
 #include "trace/trace-hw_misc_pciemu.h"
-
-/*Generate this file if it is not created automatically.
- * ./scripts/tracetool.py --backend=log,simple,ust --group=hw_misc_pciemu --format=h hw/misc/pciemu/trace-events trace/trace-hw_misc_pciemu.h
-*/
